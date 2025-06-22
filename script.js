e25dff4c8ae41536d1af887d754f09aa89ca821f
Mahesh Bhatt <141926871+adamandeveddbstudios@users.noreply.github.com>
2025-06-22 11:13:56 +0530

    new update.

--- script.js
+++ script.js
@@ -1,260 +1,114 @@
-var container;
-var content;
-var bgExit;
+// gsap.registerPlugin(SplitText);
 var startTime;
-var endTime;
-var legalView;
-var legalButton;
-var root;
+var tl;
+// Init tricggered by onLoad in Body tag
+function init() {
+  // Set Banner duration timer
+  startTime = new Date();
 
-var tl = new TimelineMax({
-		onComplete: function(){
-			//Establish timeline will lopp to .6 second mark on complete - loop after banner "fades in" (cover autoAlpha off)
-			//tl.play(.6)
-	
-			if(repeatIndex < 1){
-				repeatIndex++;
-				tl.play(.3)
-			}
-	
-			endTime = new Date();
-			var timeDiff = endTime - startTime; //in ms
-			// strip the ms
-			timeDiff /= 1000;
-			// get seconds 
-			var seconds = timeDiff;
-			//Log time - comment out for production
-			//console.log(seconds + " seconds");
-		}
-	});	
-
-
-function getOS() {
-	var userAgent = window.navigator.userAgent,
-		platform = window.navigator?.userAgentData?.platform || window.navigator.platform,
-		macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
-		windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
-		iosPlatforms = ['iPhone', 'iPad', 'iPod'],
-		os = null;
-  
-	if (macosPlatforms.indexOf(platform) !== -1) {
-	  os = 'Mac OS';
-	} else if (iosPlatforms.indexOf(platform) !== -1) {
-	  os = 'iOS';
-	} else if (windowsPlatforms.indexOf(platform) !== -1) {
-	  os = 'Windows';
-	} else if (/Android/.test(userAgent)) {
-	  os = 'Android';
-	} else if (/Linux/.test(platform)) {
-	  os = 'Linux';
-	}
-  
-	return os;
-}
-document.querySelector('body').className = getOS();
-
-
-//Start the creative
-startAd = function (e) {
-	//Assign elements
-	container = document.getElementById('banner_container_dc');
-	content = document.getElementById('content_dc');
-	bgExit1 = document.getElementById('backgroundExit1');
-	legalView = document.getElementById('legal-view');
-	legalButton = document.getElementById('legal-button');
-
-
-	//Add listeners
-	addListeners();
-
-	container.style.display = "block";
-
-
-	//Establish start date to record display running time later
-	startTime = new Date();
-
-	//Set specific legal height for nicer legal animation
-
-
-	/* root = document.querySelector(':root');
-
-	console.log(`legal height before: ${legalView.offsetHeight}`)
-	
-	
-	
-	root.style.setProperty('--legal-height' , legalView.offsetHeight)
-	document.documentElement.style.setProperty('--legal-height' , legalView.offsetHeight)
-	
-	console.log(`legal height after: ${legalView.offsetHeight}`)
-	
-	console.log(`legal official: ${ getComputedStyle(root).getPropertyValue('--legal-height')}`)
-	console.log(`margin top official: ${ getComputedStyle(legalView).getPropertyValue('margin-top')}`)
- */
-	
-
-	//Lets buils this ish
-	buildTimeline(this);
-	
-	
-
-};
-
-//Add Event Listeners
-addListeners = function (e) {
-	bgExit1.addEventListener('touchEnd', bgExitHandler, false);
-	bgExit1.addEventListener('click', bgExitHandler, false);
-
-	legalButton.addEventListener('touchEnd', legalhandler, false);
-	legalButton.addEventListener('click', legalhandler, false);
-
-
-	/* $legalButton.on('click', function() {
-        if ($legalView.hasClass("open")) {
-            $legalView.removeClass("open");
-            $legalButton.removeClass("open");
-        } else {
-            $legalView.addClass("open");
-            $legalButton.addClass("open");
-        }
-    }); */
-};
-
-addHoverListener = function(e) {
-	
+  // Set Global Timeline
+  tl = new TimelineMax({ onComplete: endTime });
+  setRollover();
+  animate();
 }
 
-//Add exits
-bgExitHandler = function (e) {
-	window.open(window.clickTag);
-};
+// Split by words and chars
+const split = new SplitText("#text1", { type: "words,chars" });
 
-legalhandler = function(e){
-	if(legalView.classList.contains('open')){
-		legalView.classList.remove('open');
-		legalButton.classList.remove('open');
-		console.log(`Margin top: ${  getComputedStyle(legalView).getPropertyValue('margin-top') }`)
-		console.log(`Margin top alt: ${  legalView.style.marginTop }`)
-	}else{
-		legalView.classList.add('open');
-		legalButton.classList.add('open');
-		console.log(`Margin top: ${  getComputedStyle(legalView).getPropertyValue('margin-top') }`)
-		console.log(`Margin top alt: ${  legalView.style.marginTop }`)
-	}
-}
-
-//Wait for the content to load to call the start of the ad
-window.onload = function () {
-	startAd();
-};	
-
-bgOverHandler = function(e){
-	console.log("BG Handler")
-	if (e.type == 'mouseover'){
-		//TweenMax.fromTo("#cta .shine", 1, {x: -200 }, {x: 200, ease: Sine.easeInOut});
-		
-	}
-	if (e.type == 'mouseout'){
-		
-	}
-}
-
-onHover = function(e){
-	
-}
-
-
-buildTimeline = function(e){
-
-	var text1 = new SplitText("#text-1", {type:"chars,words", position:"relative"});
-	var text2 = new SplitText("#text-2", {type:"chars,words", position:"relative"});
-	var text3 = new SplitText("#text-3", {type:"chars,words", position:"relative"});
-
-
-	gsap.config({nullTargetWarn:false});
-
-	var repeatIndex = 0;
-
-	
-	
-	var staggerDelay = .1;
-	var staggerDuration = 1.5;
-gsap.set(text1.chars, {
-  scale: 1,
-  x: 0,
-  y: 0,
-  opacity: 1,
-  transformOrigin: "center center"
+// Wrap each char in a span with class "char"
+split.chars = split.chars.map((el, index) => {
+  const span = document.createElement("span");
+  span.className = "char";
+  span.textContent = el.textContent;
+  span.style.display = "inline-block";
+  span.style.opacity = 0;
+  el.replaceWith(span);
+  return span;
 });
-	tl.to("#cover", .3, {autoAlpha: 0})
 
-	
-	.addLabel('t1in', "+=.5")
+// Wrap each word in a span with class "word"
+// split.words = split.words.map((el, index) => {
+//   const div = document.createElement("div");
+//   div.className = "word";
+//   div.textContent = el.textContent;
+//   div.style.display = "inline-block";
+//   el.replaceWith(div);
+//   return div;
+// });
 
-	// .from('.roofline', 1.2, {width: 0}, 't1in+=.6')
+// function convertSplitTextDivsToSpans(split) {
+//   split.chars = split.chars.map((el) => {
+//     const span = document.createElement("span");
+//     span.className = el.className;
+//     span.textContent = el.textContent;
+//     span.style.display = "inline-block";
+//     span.style.opacity = 0;
+//     el.replaceWith(span);
+//     return span;
+//   });
 
+//   split.words = split.words.map((el) => {
+//     const span = document.createElement("span");
+//     span.className = el.className;
+//     span.textContent = el.textContent;
+//     span.style.display = "inline-block";
+//     el.replaceWith(span);
+//     return span;
+//   });
 
-	// .from('#logo', .8, {right: '-=80%', opacity: 0}, 't1in+=.2')
-	// .from('#cta', .8, { top: '-=80%', opacity: 0}, 't1in+=.4')
+//   return split;
+// }
 
-	
-	.staggerFrom(text1.chars, staggerDuration, {alpha: 0, ease: Sine.ease}, staggerDelay, "t1in")
-	// .from("#text-1", .9, { top: "-=80"}, "t1in")
+function animate() {
+	tl.set(split.chars, { autoAlpha: 0 });
+  tl.staggerTo( split.chars, 1.5, { autoAlpha: 1, ease: Sine.ease }, 0.1, 0.5 );
+  //   tl.set(["#main_content"], { autoAlpha: 1, force3D: true });
+  //   tl.set(["#cta"], { force3D: false, rotation: 0.001 });
 
-
-	.addLabel('t1out', "+=1.5")
-
-	.staggerTo(text1.chars, staggerDuration, {alpha: 0, ease: Sine.easeOut}, staggerDelay, "t1out")
-	// .to("#text-1", .4, { top: "-=80"}, "t1out")
-	
-	.addLabel('t2in', "-=1.5")
-
-	.staggerFrom(text2.chars, staggerDuration, {alpha: 0, ease: Sine.ease}, staggerDelay, "t2in")
-	// .from("#text-2", .4, { top: "-=80"}, "t2in")
-
-	.addLabel('t2out', "+=1.5")
-
-	.staggerTo([text2.chars], staggerDuration, {alpha: 0, ease: Sine.easeOut}, staggerDelay, "t2out")
-	// .to("#text-2", .4, { top: "-=80"}, "t2out")
-
-	.addLabel('t3in', "-=1.5")
-
-	.staggerFrom(text3.chars, staggerDuration, {alpha: 0, ease: Sine.ease}, staggerDelay, "t3in")
-	// .from("#text-3", .4, { top: "-=80"}, "t3in")
-
-	
-
-
-	.to('#cta_1', .5, {transform: 'scale(1.1)', ease: Back.easeOut}, 't3in+=2.2')
-	.to('#cta_1', .3, {transform: 'scale(1)', ease: Back.easeOut}, 't3in+=2.7')
-	
-
-	.to("#made-up-loop-buffer", 4, {backgroundPositionY: "-=10", onStart: function(){
-		if(repeatIndex > 0){
-			tl.pause();
-			endTime = new Date();
-			var timeDiff = endTime - startTime; //in ms
-			// strip the ms
-			timeDiff /= 1000;
-			// get seconds 
-			var seconds = timeDiff;
-			//Log time - comment out for production
-			console.log(seconds + " seconds");
-		}
-	}})
-
-
-	.addLabel('out', "+=0")
-	.to('.bar', .8, {opacity: 1}, '+=0')
-	// .to('#logo', .4, {opacity: 0}, 'out')
-	.to('#cta_1', .4, {opacity: 0}, 'out')
-	.to('#text-3', .4, { opacity: 0}, 'out')
-	.to('.roofline',.6, {opacity: 0}, 'out')
-	.to("#image-1", 1, {opacity:1}, "out+=.4")
-	//.staggerTo(text2.chars, staggerDuration, {alpha: 0, ease: Sine.easeOut}, staggerDelay, "out")
-	.delay(.5)
-
-
-	tl.timeScale(3.8);
+  // .staggerFrom(
+  //   text1Split.chars,
+  //   staggerDuration,
+  //   { autoAlpha: 0, ease: Sine.easeInOut },
+  //   staggerDelay,
+  //   "t1in"
+  // )
+  // .to("#text-2", 0.5, { autoAlpha: 1, ease: "power1.in" }, 3.8)
+  // .to("#cta", 0.5, { autoAlpha: 1, ease: "power1.in" }, "+=1")
+  // .to("#car", 4, { scale: 1, ease: "power1.in", force3D: true }, 0);
 }
 
+function endTime() {
+  // show total banner animation time in browser console.
+  var endTime = new Date();
 
+  console.log(
+    "Animation duration: " + (endTime - startTime) / 1000 + " seconds"
+  );
+}
+
+function setRollover() {
+  document
+    .getElementById("default_exit")
+    .addEventListener("mouseover", default_over, false);
+  document
+    .getElementById("default_exit")
+    .addEventListener("mouseout", default_out, false);
+}
+
+function default_over(event) {
+  TweenMax.to(["#cta-roll"], 0.3, {
+    autoAlpha: 1,
+    ease: Power1.easeOut,
+    delay: 0,
+  });
+}
+
+function default_out(event) {
+  TweenMax.to(["#cta-roll"], 0.3, {
+    autoAlpha: 0,
+    ease: Power1.easeOut,
+    delay: 0,
+  });
+}
+
+// sbdhjbsdjcbsjdbcj
